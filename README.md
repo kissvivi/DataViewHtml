656f6a7037449684cd2a47ca091f08831bf71d28
unknown <1150622459@qq.com>
2020-05-11 23:07:40 +0800

    frist commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# HTML5扁平化响应式登录界面
+
+#### 介绍
+基于前端框架Bootstrap4 开发的HTML5扁平化响应式登录界面。
+
+#### 软件架构
+Bootstrap4
+
+![HTML5扁平化响应式登录界面](https://images.gitee.com/uploads/images/2018/1221/215152_241d2b1f_2024903.png "html_login.png")
---
+++ config/config.js
@@ -0,0 +1,19 @@
+/**
+ */
+var WebAPI = {
+
+	// 登录
+	"login": "http://127.0.0.1:6666 ",
+	
+	//用户管理
+	//"yhgl": location.protocol + "//" + location.host + "/Hanzai/",
+	
+	//消息管理
+	//"xxgl": location.protocol + "//" + location.host + "/Hanzai/",
+	
+	//数据管理
+	//"sjgl": location.protocol + "//" + location.host + "/Hanzai/",
+	
+	//前端首页路径
+	//"html":location.protocol + "//" + location.host +"/ganhan/home/"
+};
